5453fad19fce88d0f873405343462c321a12185b
maleyran <maleyran.ren@hotmail.fr>
2022-03-08 08:34:14 +0100

    modification index.php

--- index.php
+++ index.php
@@ -1,24 +1,17 @@
 <?php 
-
 session_start();
 
 require_once __DIR__ . '/vendor/autoload.php';
-
 try {
-
     $controllerFront = new \Projet\Controllers\FrontController();
 
     if(isset($_GET['action'])) {
+} else {
 
-    
-    } else {
-
-        $controllerFront->home();
-    }
+    $controllerFront->home();
+}
  
-
-
-} catch(Exception $e) {
+} catch (Exception $e) {
 
     require 'app/Views/Front/error.php';
     
